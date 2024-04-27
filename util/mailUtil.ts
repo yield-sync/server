6b5a4a4eb8acfb9a23efffdcc5934da037df8934
harppon <aleem.ahmed2015@gmail.com>
2024-04-26 20:01:55 -0400

    Mailer util functions

--- util/mailUtil.ts
+++ util/mailUtil.ts
@@ -1,38 +1,81 @@
 // [import-personal]
 import config from "../config"
+import { validateEmail } from "./validationUtil"
 
 // [require]
-const SibApiV3Sdk = require('sib-api-v3-typescript');
+const brevo = require('@getbrevo/brevo');
 
 
-let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const defaultClient = brevo.ApiClient.instance;
 
-let apiKey = apiInstance.authentications['apiKey'];
-
+let apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = config.api.sendinblueKey;
 
-let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+const API_INSTANCE = new brevo.TransactionalEmailsApi();
 
-sendSmtpEmail.subject = "My {{params.subject}}";
-sendSmtpEmail.htmlContent = "<html><body><h1>This is my first transactional email {{params.parameter}}</h1></body></html>";
-sendSmtpEmail.sender = {"name":"John Doe","email":"example@example.com"};
-sendSmtpEmail.to = [{"email":"example@example.com","name":"Jane Doe"}];
-sendSmtpEmail.cc = [{"email":"example2@example2.com","name":"Janice Doe"}];
-sendSmtpEmail.bcc = [{"name":"John Doe","email":"example@example.com"}];
-sendSmtpEmail.replyTo = {"email":"replyto@domain.com","name":"John Doe"};
-sendSmtpEmail.headers = {"Some-Custom-Name":"unique-id-1234"};
-sendSmtpEmail.params = {"parameter":"My param value","subject":"New Subject"};
 
-apiInstance.sendTransacEmail(sendSmtpEmail).then(
-	function(data)
+export const sendRecoveryEmail = (to: string) =>
+{
+	if (!validateEmail(to))
 	{
-		console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-	},
-	function(error)
-	{
-		console.error(error);
+		return;
 	}
-);
+
+	let sendSmtpEmail = new brevo.SendSmtpEmail();
+
+	sendSmtpEmail.subject = "Recover Your Account";
+
+	sendSmtpEmail.htmlContent = `
+	<html>
+		<body>
+			<h1>Recover Your Account</h1>
+			<p>
+				If you did NOT request to recover your account please ignore this email.
+				Do NOT share with anything they may be trying to get unauthorized access to your account.
+			</p>
+		</body>
+	</html>
+	`;
+
+	sendSmtpEmail.sender = {
+		"name": "John Doe",
+		"email": "example@example.com"
+	};
+
+	sendSmtpEmail.to = [
+		{
+			"email": to
+		}
+	];
+
+	sendSmtpEmail.bcc = [];
+
+	sendSmtpEmail.replyTo = {
+		"email": "replyto@domain.com",
+		"name": "John Doe"
+	};
+
+	sendSmtpEmail.headers = {
+		"Some-Custom-Name": "unique-id-1234"
+	};
+
+	sendSmtpEmail.params = {
+		"parameter": "My param value",
+		"subject": "New Subject"
+	};
+
+	API_INSTANCE.sendTransacEmail(sendSmtpEmail).then(
+		function(data)
+		{
+			console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+		},
+		function(error)
+		{
+			console.error(error);
+		}
+	);
+};
+
 
 export default async (email: string) =>
 {
