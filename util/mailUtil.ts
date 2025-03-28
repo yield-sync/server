b888dcac2891b070083f2cbd5b33102d670ff288
aleem ahmed <61424530+aleem-ahmed@users.noreply.github.com>
2025-03-28 00:22:50 -0400

    brevo

--- util/mailUtil.ts
+++ util/mailUtil.ts
@@ -1,6 +1,8 @@
 import config from "../config";
 import { validateEmail } from "./validation";
 
+const brevo = require("@getbrevo/brevo");
+
 
 function getRecoveryEmail(recoveryPassword: string = "")
 {
@@ -8,16 +10,16 @@ function getRecoveryEmail(recoveryPassword: string = "")
 		subject: "Recover Your Account",
 		body: `
 			<html>
-			<body>
-				<h1>Recover Your Account</h1>
-				<p>
-					If you did NOT request to recover your account please ignore this email.
-					Do NOT share with anything they may be trying to get unauthorized access to your account.
+				<body>
+					<h1>Recover Your Account</h1>
+					<p>
+						If you did NOT request to recover your account please ignore this email.
+						Do NOT share with anything they may be trying to get unauthorized access to your account.
 
-					To recover your account please visit the following link:
-					<a href="${config.app.domain}/recover/${recoveryPassword}">
-				</p>
-			</body>
+						To recover your account please visit the following link:
+						<a href="${config.app.domain}/recover/${recoveryPassword}">
+					</p>
+				</body>
 			</html>
 		`,
 	};
@@ -33,21 +35,45 @@ export const sendRecoveryEmail = async (to: string) =>
 
 	const email = getRecoveryEmail()
 
-	const mailchimpClient = require("@mailchimp/mailchimp_transactional")(config.api.mailchimp.key);
+	let defaultClient = brevo.ApiClient.instance;
 
-	return await mailchimpClient.messages.send({
-		message: {
-			from_email: `admin@${config.app.domain}`,
-			to: [
-				{
-					email: to,
-					type: "to",
-				},
-			],
-			subject: email.subject,
-			html: email.body,
+	let apiKey = defaultClient.authentications['apiKey'];
+
+	apiKey.apiKey = config.api.mailchimp.key;
+
+	let apiInstance = new brevo.TransactionalEmailsApi();
+	let sendSmtpEmail = new brevo.SendSmtpEmail();
+
+	sendSmtpEmail.subject = email.subject;
+	
+	sendSmtpEmail.htmlContent = email.body;
+
+	sendSmtpEmail.sender = {
+		"name": "Auto",
+		"email": `no-reply@${config.app.domain}`
+	};
+	
+	sendSmtpEmail.to = [
+		{
+			"email": to,
+			"name": "Name Here"
+		}
+	];
+	
+	sendSmtpEmail.replyTo = { "email": "example@brevo.com", "name": "sample-name" };
+	
+	sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
+	
+	sendSmtpEmail.params = { "parameter": "My param value", "subject": "common subject" };
+
+	apiInstance.sendTransacEmail(sendSmtpEmail).then(
+		function (data) {
+			console.log('API called successfully. Returned data: ' + JSON.stringify(data));
 		},
-	});
+		function (error) {
+			console.error(error);
+		}
+	);
 };
 
 export const setVerificationEmail = (to: string) =>
